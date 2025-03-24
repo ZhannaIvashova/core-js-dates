588e733daaa662fd03b8931a96bd341b4dd63e26
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-03-24 14:48:25 +0300

    feat: add tasks getCountDaysInMonth and getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -98,8 +98,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
 }
 
 /**
@@ -113,8 +114,15 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+
+  const diffInMs = end.getTime() - start.getTime();
+
+  const diffInDays = diffInMs / (1000 * 60 * 60 * 24) + 1;
+
+  return diffInDays;
 }
 
 /**
