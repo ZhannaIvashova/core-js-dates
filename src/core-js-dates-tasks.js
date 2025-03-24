af5387b5ad4d0c24fdb65086e0366b0c9eb946e8
ZhannaIvashova <zhannaklepikova90@gmail.com>
2025-03-24 15:05:06 +0300

    feat: add tasks isDateInPeriod and formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -142,8 +142,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkData = new Date(date).getTime();
+  const startData = new Date(period.start).getTime();
+  const endData = new Date(period.end).getTime();
+
+  return checkData >= startData && checkData <= endData;
 }
 
 /**
@@ -157,8 +161,20 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const options = {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+    timeZone: 'UTC',
+  };
+
+  const result = new Date(date).toLocaleString('en-US', options);
+  return result;
 }
 
 /**
